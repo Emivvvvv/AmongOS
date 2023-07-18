87ba3c40ba95eea4d89917f2436cfeacbec791bf
Emirhan TALA <emirhantala99ff99@gmail.com>
2023-07-18 18:45:08 +0300

    v0.3.1

---
+++ src/screen.rs
@@ -0,0 +1,211 @@
+const VERSION: &str = "v0.3.1";
+
+use core::fmt;
+use lazy_static::lazy_static;
+use spin::Mutex;
+use volatile::Volatile;
+
+
+lazy_static! {
+    pub static ref WRITER: Mutex<Writer> = Mutex::new(Writer {
+        column_position: 0,
+        color_code: ColorCode::new(Color::Yellow, Color::Black),
+        buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
+    });
+}
+
+
+#[allow(unused)]
+#[derive(Clone, Copy)]
+#[repr(u8)]
+pub enum Color {
+    Black = 0x0, White = 0xF,
+    Blue = 0x1, BrightBlue = 0x9,
+    Green = 0x2, BrightGreen = 0xA,
+    Cyan = 0x3, BrightCyan = 0xB,
+    Red = 0x4, BrightRed = 0xC,
+    Magenta = 0x5, BrightMagenta = 0xD,
+    Brown = 0x6, Yellow = 0xE,
+    Gray = 0x7, DarkGray = 0x8,
+}
+
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(transparent)]
+struct ColorCode(u8);
+
+impl ColorCode {
+    fn new(foreground: Color, background: Color) -> ColorCode {
+        ColorCode((background as u8) << 4 | (foreground as u8))
+    }
+}
+
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(C)]
+struct ScreenChar {
+    ascii_character: u8,
+    color_code: ColorCode,
+}
+
+
+const BUFFER_HEIGHT: usize = 25;
+const BUFFER_WIDTH: usize = 80;
+
+
+#[repr(transparent)]
+#[derive(Debug)]
+struct Buffer {
+    chars: [[Volatile<ScreenChar>; BUFFER_WIDTH]; BUFFER_HEIGHT],
+}
+
+
+pub struct Writer {
+    column_position: usize,
+    color_code: ColorCode,
+    buffer: &'static mut Buffer,
+}
+
+impl Writer {
+    pub fn write_byte(&mut self, byte: u8) {
+        match byte {
+            b'\n' => self.new_line(),
+            byte => {
+                if self.column_position >= BUFFER_WIDTH {
+                    self.new_line();
+                }
+
+                let row = BUFFER_HEIGHT - 1;
+                let col = self.column_position;
+
+                let color_code = self.color_code;
+                self.buffer.chars[row][col].write(ScreenChar {
+                    ascii_character: byte,
+                    color_code,
+                });
+                self.column_position += 1;
+            }
+        }
+    }
+
+
+    fn write_string(&mut self, s: &str) {
+        for byte in s.bytes() {
+            match byte {
+                // printable ASCII byte or newline
+                0x20..=0x7e | b'\n' => self.write_byte(byte),
+                // not part of printable ASCII range
+                _ => self.write_byte(0xfe),
+            }
+        }
+    }
+
+
+    fn new_line(&mut self) {
+        self.delete_among_os();
+        for row in 1..BUFFER_HEIGHT {
+            for col in 0..BUFFER_WIDTH {
+                let character = self.buffer.chars[row][col].read();
+                self.buffer.chars[row - 1][col].write(character);
+            }
+        }
+        self.clear_row(BUFFER_HEIGHT - 1);
+        self.column_position = 0;
+        self.draw_among_os();
+    }
+
+
+    fn clear_row(&mut self, row: usize) {
+        let blank = ScreenChar {
+            ascii_character: b' ',
+            color_code: self.color_code,
+        };
+        for col in 0..BUFFER_WIDTH {
+            self.buffer.chars[row][col].write(blank);
+        }
+    }
+
+    fn draw_among_os(&mut self) {
+        //  [x, x, x, x, x, x, y, c]
+        //  x => x(column) coordinate slots. 0s will be excluded.
+        //  y => y(row) coordinate slot,
+        //  c => Color code of the red imposter from among us's part
+        let among_os: [[isize; 8]; 9] = [
+            [38, 39, 40, -1, -1, -1, 9, 0x4],
+            [37, 38, 39, 40, 41, -1, 10, 0x4],
+            [36, 37, 38, -1, -1, -1, 11, 0x4],
+            [36, 37, 38, -1, -1, -1, 12, 0x4],
+            [36, 37, 38, 39, 40, 41, 13, 0x4],
+            [36, 37, 38, 39, 40, 41, 14, 0x4],
+            [37, 38, 40, 41, -1, -1, 15, 0x4],
+            [39, 40, 41, 42, -1, -1, 11, 0xB],
+            [39, 40, 41, 42, -1, -1, 12, 0xB]];
+
+        among_os.into_iter().for_each(|pack| {
+            self.draw_line_among_os(&pack[..6], pack[6], pack[7])
+        });
+    }
+
+    fn draw_line_among_os(&mut self, x_list: &[isize], row: isize, color: isize) {
+        let color_code = if color == 0x4 {ColorCode::new(Color::Yellow, Color::Red)}
+        else {ColorCode::new(Color::Yellow, Color::BrightCyan)};
+
+        self.draw_line(x_list, row, color_code);
+    }
+
+    fn delete_among_os(&mut self) {
+        for row in 9..=15 {
+            for col in 0..BUFFER_WIDTH {
+                let mut screen_char = self.buffer.chars[row][col].read();
+                screen_char.color_code = ColorCode::new(Color::Yellow, Color::Black);
+                self.buffer.chars[row][col].write(screen_char);
+            }
+        }
+    }
+
+    // -1 values in x_list will be omitted.
+    fn draw_line(&mut self, x_list: &[isize], row: isize, color_code: ColorCode) {
+        for col in x_list {
+            if *col == -1 { continue }
+            let mut screen_char = self.buffer.chars[row as usize][*col as usize].read();
+            screen_char.color_code = color_code;
+            self.buffer.chars[row as usize][*col as usize].write(screen_char);
+        }
+    }
+
+
+}
+
+impl fmt::Write for Writer {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        self.write_string(s);
+        Ok(())
+    }
+}
+
+
+#[macro_export]
+macro_rules! print {
+    ($($arg:tt)*) => ($crate::screen::_print(format_args!($($arg)*)));
+}
+
+
+#[macro_export]
+macro_rules! println {
+    () => ($crate::print!("\n"));
+    ($($arg:tt)*) => ($crate::print!("{}\n", format_args!($($arg)*)));
+}
+
+
+pub fn _print(args: fmt::Arguments) {
+    use core::fmt::Write;
+    WRITER.lock().write_fmt(args).unwrap();
+}
+
+pub fn welcome() {
+    println!("AMONG OS {}", VERSION);
+    println!("Created by Emirhan Tala");
+    println!();
+    print!(">>> ");
+    WRITER.lock().draw_among_os();
+}
